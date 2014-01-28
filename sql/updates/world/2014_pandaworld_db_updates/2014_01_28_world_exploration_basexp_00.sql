510d71608948938c45a1c6e84713e45f0931c3e3
Kittnz <frederik156@hotmail.com>
2014-01-28 20:39:31 +0100

    DB/Exploration: base xp for explore level information from 80-89 added.

---
+++ sql/updates/world/2014_pandaworld_db_updates/2014_01_28_world_exploration_basexp_00.sql
@@ -0,0 +1,12 @@
+-- Adding explore level information from 80-89
+INSERT INTO `exploration_basexp` (`level`, `basexp`) VALUES
+(80, 1670),
+(81, 1700),
+(82, 1750),
+(83, 1780),
+(84, 1800),
+(85, 1852),
+(86, 1892),
+(87, 1930),
+(88, 1968),
+(89, 2006);
