a0c77a709a153b5b779ecb8b2bd64b0e97798671
Warkantos <wark.junior@gmail.com>
2014-01-29 20:19:20 -0200

    Updated some headers - pandashan included

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -3,8 +3,8 @@
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
  #define _BRANCH                    "@rev_branch@"
- #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2014 TrinityCore"
+ #define VER_COMPANYNAME_STR        "MistCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2014 MistCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
