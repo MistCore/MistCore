3858d7c488d4998333034b704165c11f2ebf104e
VeraDev <laymasterlolz@gmail.com>
2014-03-12 18:10:56 +0100

    Add ."reset level command", which also fixed a console error with it missing.
    Also Removed "Unstuck" from command as we are using "Start" - fixes console error.

--- src/server/scripts/Commands/cs_reset.cpp
+++ src/server/scripts/Commands/cs_reset.cpp
@@ -37,6 +37,7 @@ public:
         {
             { "achievements",   SEC_ADMINISTRATOR,  true,  &HandleResetAchievementsCommand,     "", NULL },
             { "honor",          SEC_ADMINISTRATOR,  true,  &HandleResetHonorCommand,            "", NULL },
+			 { "level",          SEC_ADMINISTRATOR,  true,  &HandleResetLevelCommand,            "", NULL },
             { "spells",         SEC_ADMINISTRATOR,  true,  &HandleResetSpellsCommand,           "", NULL },
             { "stats",          SEC_ADMINISTRATOR,  true,  &HandleResetStatsCommand,            "", NULL },
             { "talents",        SEC_ADMINISTRATOR,  true,  &HandleResetTalentsCommand,          "", NULL },
@@ -116,6 +117,42 @@ public:
         //player->SetUInt32Value(PLAYER_FIELD_BYTES, 0xEEE00000);
         return true;
     }
+
+	static bool HandleResetLevelCommand(ChatHandler* handler, char const* args)
+	{
+		Player* target;
+		if (!handler->extractPlayerTarget((char*)args, &target))
+			return false;
+
+		if (!HandleResetStatsOrLevelHelper(target))
+			return false;
+
+		uint8 oldLevel = target->getLevel();
+
+		// set starting level
+		uint32 startLevel = target->getClass() != CLASS_DEATH_KNIGHT
+			? sWorld->getIntConfig(CONFIG_START_PLAYER_LEVEL)
+			: sWorld->getIntConfig(CONFIG_START_HEROIC_PLAYER_LEVEL);
+
+		target->_ApplyAllLevelScaleItemMods(false);
+		target->SetLevel(startLevel);
+		target->InitRunes();
+		target->InitStatsForLevel(true);
+		target->InitTaxiNodesForLevel();
+		target->InitGlyphsForLevel();
+		target->InitTalentForLevel();
+		target->SetUInt32Value(PLAYER_XP, 0);
+
+		target->_ApplyAllLevelScaleItemMods(true);
+
+		// reset level for pet
+		if (Pet* pet = target->GetPet())
+			pet->SynchronizeLevelWithOwner();
+
+		sScriptMgr->OnPlayerLevelChanged(target, oldLevel);
+
+		return true;
+	}
 
     static bool HandleResetSpellsCommand(ChatHandler* handler, char const* args)
     {
