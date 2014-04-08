9164b81d1673e578c11e06d693fa7c599a182add
silvermcd123 <mcdhiphop@hotmail.com>
2014-04-08 05:41:28 +0100

    Spawn Missing controller for Mogushan Vaults Spirit Kings fight

---
+++ sql/updates/world/2014_04_08_creature_template_02.sql
@@ -0,0 +1,3 @@
+-- Missing Controller for Spirit Kings Fight
+
+insert into `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `isActive`) values('1394860','60847','1008','6125','6125','8','4294967295','0','0','4227.55','1594.02','438.804','4.71804','300','0','0','87227400','0','0','0','0','0','0');
