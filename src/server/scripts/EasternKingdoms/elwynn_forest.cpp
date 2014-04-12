6f0fb1c4c31ccf829f961b1facd6a2eb3a10c3c6
Basti420 <Basti420@users.noreply.github.com>
2014-04-12 11:45:29 +0200

    Update elwynn_forest.cpp

--- src/server/scripts/EasternKingdoms/elwynn_forest.cpp
+++ src/server/scripts/EasternKingdoms/elwynn_forest.cpp
@@ -142,14 +142,14 @@ enum eTrainingDummySpells
     SPELL_AUTORITE		= 105361, // OnDamage
     SPELL_ASSURE		= 56641,
     SPELL_EVISCERATION		= 2098,
-    SPELL_MOT_DOULEUR_1		= 589,
+    SPELL_MOT_DOULEUR_1 	= 589,
     SPELL_MOT_DOULEUR_2 	= 124464, // Je ne sais pas si un des deux est le bon
     SPELL_NOVA          	= 122,
     SPELL_CORRUPTION_1  	= 172,
     SPELL_CORRUPTION_2  	= 87389,
     SPELL_CORRUPTION_3		= 131740,
     SPELL_PAUME_TIGR		= 100787,
-    SPELL_SEAL_OF_COMMAN	= 118215
+    SPELL_SEAL_OF_COMMAND	= 118215
 };
 
 class npc_training_dummy_start_zones : public CreatureScript
