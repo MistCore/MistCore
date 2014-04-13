96563db87c38f53d09640c5a3fa84a733cdb1d4d
silvermcd123 <mcdhiphop@hotmail.com>
2014-04-14 00:49:14 +0100

    Update creature loots for some Mogushan Vaults

---
+++ sql/updates/world/2014_04_14_creature_template.sql
@@ -0,0 +1,4 @@
+-- Missing Loot Template On Ziang - Spirit Kings - Mogushan vaults
+UPDATE creature_template SET lootid='60701' WHERE entry='60701';
+-- Mising Loot Template On Elegon - Mogushan Vaults
+UPDATE creature_template SET lootid='60410' WHERE entry='60410';
