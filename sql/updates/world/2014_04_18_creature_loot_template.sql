a63512d36c59743b80447782ae2f659a3eb443c2
silvermcd123 <mcdhiphop@hotmail.com>
2014-04-18 13:27:12 +0100

    Fixes quest 31473

---
+++ sql/updates/world/2014_04_18_creature_loot_template.sql
@@ -0,0 +1,15 @@
+-- Drops for quest 31473
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60043','87208','-100','1','0','1','1');
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60043','87209','-100','1','0','1','1');
+
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60009','87208','-100','1','0','1','1');
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60009','87209','-100','1','0','1','1');
+
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60710','87208','-100','1','0','1','1');
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60710','87209','-100','1','0','1','1');
+
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60410','87208','-100','1','0','1','1');
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60410','87209','-100','1','0','1','1');
+
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60400','87208','-100','1','0','1','1');
+INSERT INTO `creature_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES ( '60400','87209','-100','1','0','1','1');
