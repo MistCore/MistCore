e5344cee173d89c3afd503b6d60c32862aa8486e
avenegerweb <Avengerweb@yandex.ru>
2014-05-13 18:34:41 +0400

    DB: Parse right loot for some creatures.

---
+++ sql/updates/world/2014_05_12_01_creature_loot_template.sql
@@ -0,0 +1,243 @@
+DELETE FROM `creature_loot_template` WHERE `entry`=4832;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(4832,5881,68.8949, 1, 0, 1, 1),-- Head of Kelris
+	(4832,1155,36.8768, 1, 0, 1, 1),-- Rod of the Sleepwalker
+	(4832,6903,36.0254, 1, 0, 1, 1),-- Gaze Dreamer Pants
+	(4832,2592,8.7670, 1, 0, 1, 1),-- Wool Cloth
+	(4832,2589,2.8961, 1, 0, 1, 1),-- Linen Cloth
+	(4832,3770,1.9832, 1, 0, 1, 1),-- Mutton Chop
+	(4832,1205,0.9642, 1, 0, 1, 1),-- Melon Juice
+	(4832,71635,0.9027, 1, 0, 1, 1),-- Imbued Crystal
+	(4832,4306,0.7061, 1, 0, 1, 1),-- Silk Cloth
+	(4832,929,0.5761, 1, 0, 1, 1),-- Healing Potion
+	(4832,1822,0.3864, 1, 0, 1, 1),-- Cedar Walking Stick
+	(4832,1821,0.3129, 1, 0, 1, 1),-- Warped Blade
+	(4832,1818,0.3094, 1, 0, 1, 1),-- Standard Claymore
+	(4832,1206,0.2872, 1, 0, 1, 1),-- Moss Agate
+	(4832,2782,0.2735, 1, 0, 1, 1),-- Mishandled Recurve Bow
+	(4832,1819,0.2718, 1, 0, 1, 1),-- Gouging Pick
+	(4832,3385,0.2599, 1, 0, 1, 1),-- Lesser Mana Potion
+	(4832,2765,0.2530, 1, 0, 1, 1),-- Hunting Knife
+	(4832,1824,0.2496, 1, 0, 1, 1),-- Shiny War Axe
+	(4832,1823,0.2359, 1, 0, 1, 1),-- Bludgeoning Cudgel
+	(4832,2783,0.2359, 1, 0, 1, 1),-- Shoddy Blunderbuss
+	(4832,1800,0.2240, 1, 0, 1, 1),-- Rawhide Pants
+	(4832,1820,0.2171, 1, 0, 1, 1),-- Wooden Maul
+	(4832,2220,0.2086, 1, 0, 1, 1),-- Box Shield
+	(4832,1777,0.2069, 1, 0, 1, 1),-- Brocade Shoulderpads
+	(4832,1772,0.1915, 1, 0, 1, 1),-- Brocade Shoes
+	(4832,1775,0.1795, 1, 0, 1, 1),-- Brocade Gloves
+	(4832,2219,0.1744, 1, 0, 1, 1); -- Small Round Shield
+    
+DELETE FROM `creature_loot_template` WHERE `entry`=4278;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(4278,6320,23.5833, 1, 0, 1, 1),-- Commander's Crest
+	(4278,3191,23.4228, 1, 0, 1, 1),-- Arced War Axe
+	(4278,63447,8.4761, 1, 0, 1, 1),-- Breastplate of the Stilled Heart
+	(4278,63449,8.2592, 1, 0, 1, 1),-- Thieving Spaulders
+	(4278,63448,8.1456, 1, 0, 1, 1),-- Springvale's Cloak
+	(4278,63445,8.0639, 1, 0, 1, 1),-- Arced War Axe
+	(4278,63446,7.8958, 1, 0, 1, 1),-- Haunting Footfalls
+	(4278,2592,6.9766, 1, 0, 1, 1),-- Wool Cloth
+	(4278,60879,6.7869, 1, 0, 1, 1),-- Commander's Holy Symbol
+	(4278,60880,2.5202, 1, 0, 1, 1),-- Springvale's Sharpening Stone
+	(4278,2589,2.2282, 1, 0, 1, 1),-- Linen Cloth
+	(4278,4606,1.5118, 1, 0, 1, 1),-- Spongy Morel
+	(4278,1205,0.7531, 1, 0, 1, 1),-- Melon Juice
+	(4278,4306,0.6009, 1, 0, 1, 1),-- Silk Cloth
+	(4278,929,0.4301, 1, 0, 1, 1),-- Healing Potion
+	(4278,3385,0.3155, 1, 0, 1, 1),-- Lesser Mana Potion
+	(4278,71715,0.2920, 1, 0, 1, 1),-- A Treatise on Strategy
+	(4278,2782,0.2601, 1, 0, 1, 1),-- Mishandled Recurve Bow
+	(4278,1819,0.2282, 1, 0, 1, 1),-- Gouging Pick
+	(4278,1206,0.2216, 1, 0, 1, 1),-- Moss Agate
+	(4278,2765,0.2075, 1, 0, 1, 1),-- Hunting Knife
+	(4278,1818,0.2038, 1, 0, 1, 1),-- Standard Claymore
+	(4278,1824,0.2019, 1, 0, 1, 1),-- Shiny War Axe
+	(4278,1822,0.1925, 1, 0, 1, 1),-- Cedar Walking Stick
+	(4278,1821,0.1916, 1, 0, 1, 1),-- Warped Blade
+	(4278,2783,0.1831, 1, 0, 1, 1),-- Shoddy Blunderbuss
+	(4278,2220,0.1822, 1, 0, 1, 1),-- Box Shield
+	(4278,1823,0.1728, 1, 0, 1, 1),-- Bludgeoning Cudgel
+	(4278,1799,0.1559, 1, 0, 1, 1),-- Rawhide Gloves
+	(4278,1776,0.1549, 1, 0, 1, 1),-- Brocade Pants
+	(4278,1820,0.1540, 1, 0, 1, 1),-- Wooden Maul
+	(4278,1778,0.1474, 1, 0, 1, 1),-- Brocade Vest
+	(4278,1777,0.1362, 1, 0, 1, 1),-- Brocade Shoulderpads
+	(4278,1798,0.1352, 1, 0, 1, 1),-- Rawhide Cloak
+	(4278,1797,0.1343, 1, 0, 1, 1),-- Rawhide Bracers
+	(4278,1796,0.1343, 1, 0, 1, 1),-- Rawhide Boots
+	(4278,1774,0.1333, 1, 0, 1, 1),-- Brocade Cloak
+	(4278,3379,0.1305, 1, 0, 1, 1),-- Brocade Bracers
+	(4278,1795,0.1286, 1, 0, 1, 1),-- Rawhide Belt
+	(4278,3378,0.1277, 1, 0, 1, 1),-- Brocade Belt
+	(4278,1775,0.1249, 1, 0, 1, 1),-- Brocade Gloves
+	(4278,1748,0.1174, 1, 0, 1, 1),-- Linked Chain Bracers
+	(4278,1751,0.1108, 1, 0, 1, 1),-- Linked Chain Pants
+	(4278,1752,0.1070, 1, 0, 1, 1),-- Linked Chain Shoulderpads
+	(4278,1802,0.1070, 1, 0, 1, 1),-- Rawhide Tunic
+	(4278,2219,0.1070, 1, 0, 1, 1),-- Small Round Shield
+	(4278,1800,0.1052, 1, 0, 1, 1),-- Rawhide Pants
+	(4278,1772,0.1023, 1, 0, 1, 1),-- Brocade Shoes
+	(4278,1749,0.1014, 1, 0, 1, 1),-- Linked Chain Cloak
+	(4278,4576,0.0967, 1, 0, 1, 1),-- Light Bow
+	(4278,1753,0.0930, 1, 0, 1, 1); -- Linked Chain Vest
+    
+DELETE FROM `creature_loot_template` WHERE `entry`=11937;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(11937,6297,89.0457, 1, 0, 1, 1),-- Old Skull
+	(11937,25418,10.0314, 1, 0, 1, 1),-- Razor Sharp Fang
+	(11937,4784,0.2785, 1, 0, 1, 1),-- Lifeless Stone
+	(11937,3781,0.1802, 1, 0, 1, 1),-- Broad Claymore
+	(11937,3783,0.1775, 1, 0, 1, 1),-- Light Scimitar
+	(11937,4553,0.1720, 1, 0, 1, 1),-- Jagged Piece of Stone
+	(11937,3782,0.1693, 1, 0, 1, 1),-- Large War Club
+	(11937,3793,0.1611, 1, 0, 1, 1),-- Interlaced Boots
+	(11937,3779,0.1584, 1, 0, 1, 1),-- Hefty War Axe
+	(11937,8746,0.1529, 1, 0, 1, 1),-- Interlaced Cowl
+	(11937,3787,0.1529, 1, 0, 1, 1),-- Stone Club
+	(11937,3778,0.1502, 1, 0, 1, 1),-- Taut Compound Bow
+	(11937,3785,0.1474, 1, 0, 1, 1),-- Keen Axe
+	(11937,3780,0.1447, 1, 0, 1, 1),-- Long-Barreled Musket
+	(11937,3784,0.1283, 1, 0, 1, 1),-- Metal Stave
+	(11937,4634,0.1256, 1, 0, 1, 1),-- Iron Lockbox
+	(11937,3786,0.1229, 1, 0, 1, 1),-- Shiny Dirk
+	(11937,3796,0.1201, 1, 0, 1, 1),-- Interlaced Gloves
+	(11937,3798,0.1092, 1, 0, 1, 1),-- Interlaced Shoulderpads
+	(11937,3799,0.1010, 1, 0, 1, 1),-- Interlaced Vest
+	(11937,3817,0.0983, 1, 0, 1, 1),-- Reinforced Buckler
+	(11937,3802,0.0983, 1, 0, 1, 1),-- Hardened Leather Bracers
+	(11937,3797,0.0983, 1, 0, 1, 1),-- Interlaced Pants
+	(11937,1529,0.0956, 1, 0, 1, 1),-- Jade
+	(11937,3805,0.0956, 1, 0, 1, 1),-- Hardened Leather Pants
+	(11937,3816,0.0928, 1, 0, 1, 1),-- Reflective Heater
+	(11937,3812,0.0901, 1, 0, 1, 1),-- Double Mail Gloves
+	(11937,3803,0.0874, 1, 0, 1, 1),-- Hardened Cloak
+	(11937,8747,0.0846, 1, 0, 1, 1),-- Hardened Leather Helm
+	(11937,3806,0.0846, 1, 0, 1, 1),-- Hardened Leather Shoulderpads
+	(11937,7912,0.0819, 1, 0, 1, 1),-- Solid Stone
+	(11937,3792,0.0792, 1, 0, 1, 1),-- Interlaced Belt
+	(11937,3795,0.0765, 1, 0, 1, 1),-- Interlaced Cloak
+	(11937,3808,0.0737, 1, 0, 1, 1),-- Double Mail Belt
+	(11937,3811,0.0710, 1, 0, 1, 1),-- Double-Stitched Cloak
+	(11937,3809,0.0655, 1, 0, 1, 1),-- Double Mail Boots
+	(11937,3794,0.0628, 1, 0, 1, 1),-- Interlaced Bracers
+	(11937,3807,0.0546, 1, 0, 1, 1),-- Hardened Leather Tunic
+	(11937,3801,0.0546, 1, 0, 1, 1),-- Hardened Leather Boots
+	(11937,3804,0.0546, 1, 0, 1, 1),-- Hardened Leather Gloves
+	(11937,3800,0.0546, 1, 0, 1, 1),-- Hardened Leather Belt
+	(11937,3814,0.0546, 1, 0, 1, 1),-- Double Mail Shoulderpads
+	(11937,8748,0.0519, 1, 0, 1, 1),-- Double Mail Coif
+	(11937,3815,0.0519, 1, 0, 1, 1),-- Double Mail Vest
+	(11937,3810,0.0491, 1, 0, 1, 1),-- Double Mail Bracers
+	(11937,5213,0.0464, 1, 0, 1, 1),-- Scorching Wand
+	(11937,3185,0.0410, 1, 0, 1, 1),-- Acrobatic Staff
+	(11937,4552,0.0355, 1, 0, 1, 1),-- Smooth Stone Chip
+	(11937,2080,0.0328, 1, 0, 1, 1),-- Hillborne Axe
+	(11937,14416,0.0328, 1, 0, 1, 1),-- Stonecloth Bindings
+	(11937,14411,0.0300, 1, 0, 1, 1),-- Stonecloth Gloves
+	(11937,15286,0.0246, 1, 0, 1, 1),-- Long Redwood Bow
+	(11937,15226,0.0246, 1, 0, 1, 1),-- Giant Club
+	(11937,15540,0.0218, 1, 0, 1, 1),-- Wicked Chain Helmet
+	(11937,3813,0.0218, 1, 0, 1, 1),-- Double Mail Pants
+	(11937,1705,0.0191, 1, 0, 1, 1),-- Lesser Moonstone
+	(11937,15130,0.0191, 1, 0, 1, 1),-- Cutthroat's Vest
+	(11937,15536,0.0191, 1, 0, 1, 1),-- Wicked Chain Chestpiece
+	(11937,13063,0.0164, 1, 0, 1, 1),-- Starfaller
+	(11937,14214,0.0164, 1, 0, 1, 1),-- Vital Boots
+	(11937,14584,0.0164, 1, 0, 1, 1),-- Dokebi Hat
+	(11937,4353,0.0164, 1, 0, 1, 1),-- Pattern: Spider Belt
+	(11937,14222,0.0164, 1, 0, 1, 1),-- Geomancer's Gloves
+	(11937,4350,0.0137, 1, 0, 1, 1),-- Pattern: Spider Silk Slippers
+	(11937,14401,0.0137, 1, 0, 1, 1),-- Resilient Cap
+	(11937,14404,0.0137, 1, 0, 1, 1),-- Resilient Leggings
+	(11937,14201,0.0137, 1, 0, 1, 1),-- Thistlefur Mantle
+	(11937,14200,0.0137, 1, 0, 1, 1),-- Thistlefur Cap
+	(11937,15358,0.0137, 1, 0, 1, 1),-- Headhunter's Woolies
+	(11937,15552,0.0137, 1, 0, 1, 1),-- Thick Scale Shield
+	(11937,2277,0.0027, 1, 0, 1, 1),-- Necromancer Leggings
+	(11937,14209,0.0027, 1, 0, 1, 1),-- Vital Sash
+	(11937,15364,0.0027, 1, 0, 1, 1),-- Trickster's Cloak
+	(11937,1465,0.0027, 1, 0, 1, 1); -- Tigerbane
+    
+DELETE FROM `creature_loot_template` WHERE `entry`=18373;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(18373,28558,39.4085, 1, 0, 1, 1),-- Spirit Shard
+	(18373,23572,23.1264, 1, 0, 1, 1),-- Primal Nether
+	(18373,21877,12.6522, 1, 0, 1, 1),-- Netherweave Cloth
+	(18373,27412,9.7632, 1, 0, 1, 1),-- Ironstaff of Regeneration
+	(18373,29257,9.5825, 1, 0, 1, 1),-- Sash of Arcane Visions
+	(18373,27415,9.3749, 1, 0, 1, 1),-- Darkguard Face Mask
+	(18373,27416,9.2882, 1, 0, 1, 1),-- Fetish of the Fallen
+	(18373,29244,9.1105, 1, 0, 1, 1),-- Wave-Song Girdle
+	(18373,27413,8.8894, 1, 0, 1, 1),-- Ring of the Exarchs
+	(18373,27411,8.3845, 1, 0, 1, 1),-- Slippers of Serenity
+	(18373,27414,8.0499, 1, 0, 1, 1),-- Mok'Nathal Beast-Mask
+	(18373,29354,4.7532, 1, 0, 1, 1),-- Light-Touched Stole of Altruism
+	(18373,27871,4.3080, 1, 0, 1, 1),-- Maladaar's Blessed Chaplet
+	(18373,27523,4.1945, 1, 0, 1, 1),-- Exarch's Diamond Band
+	(18373,27869,3.8479, 1, 0, 1, 1),-- Soulpriest's Ring of Resolve
+	(18373,27870,3.7792, 1, 0, 1, 1),-- Doomplate Legguards
+	(18373,27872,3.7777, 1, 0, 1, 1),-- The Harvester of Souls
+	(18373,27867,3.5507, 1, 0, 1, 1),-- Boots of the Unjust
+	(18373,71637,2.7844, 1, 0, 1, 1),-- Mysterious Grimoire
+	(18373,30586,2.3153, 1, 0, 1, 1),-- Purified Tanzanite
+	(18373,27855,2.2347, 1, 0, 1, 1),-- Mag'har Grainbread
+	(18373,30588,2.1525, 1, 0, 1, 1),-- Potent Fire Opal
+	(18373,30587,1.9762, 1, 0, 1, 1),-- Champion's Fire Opal
+	(18373,27860,1.0919, 1, 0, 1, 1),-- Purified Draenic Water
+	(18373,33836,0.7364, 1, 0, 1, 1),-- The Exarch's Soul Gem
+	(18373,22829,0.7066, 1, 0, 1, 1),-- Super Healing Potion
+	(18373,22832,0.3376, 1, 0, 1, 1),-- Super Mana Potion
+	(18373,25397,0.2241, 1, 0, 1, 1),-- Eroded Axe
+	(18373,25405,0.2166, 1, 0, 1, 1),-- Rusted Musket
+	(18373,31901,0.2106, 1, 0, 1, 1),-- Ace of Furies
+	(18373,31952,0.1897, 1, 0, 1, 1),-- Khorium Lockbox
+	(18373,31910,0.1867, 1, 0, 1, 1),-- Ace of Lunacy
+	(18373,25403,0.1793, 1, 0, 1, 1),-- Sharpened Stilleto
+	(18373,25402,0.1598, 1, 0, 1, 1),-- The Stoppable Force
+	(18373,31882,0.1329, 1, 0, 1, 1),-- Ace of Blessings
+	(18373,31892,0.1150, 1, 0, 1, 1),-- Ace of Storms
+	(18373,25398,0.0822, 1, 0, 1, 1),-- Stone Reaper
+	(18373,25404,0.0777, 1, 0, 1, 1),-- Dense War Staff
+	(18373,25406,0.0777, 1, 0, 1, 1),-- Broken Longbow
+	(18373,25401,0.0717, 1, 0, 1, 1),-- Corroded Mace
+	(18373,25399,0.0642, 1, 0, 1, 1),-- Deteriorating Blade
+	(18373,25380,0.0568, 1, 0, 1, 1),-- Corroded Mail Shoulderpads
+	(18373,25400,0.0553, 1, 0, 1, 1),-- Tarnished Claymore
+	(18373,25363,0.0508, 1, 0, 1, 1),-- Decaying Leather Pants
+	(18373,25057,0.0493, 1, 0, 1, 1),-- Amber Band
+	(18373,25376,0.0493, 1, 0, 1, 1),-- Corroded Mail Bracers
+	(18373,25022,0.0373, 1, 0, 1, 1),-- Warlord's Iron-Girdle
+	(18373,25210,0.0299, 1, 0, 1, 1); -- Double-Bladed Axe
+    
+DELETE FROM `creature_loot_template` WHERE `entry`=4425;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(4425,6695,31.3276, 1, 0, 1, 1),-- Stygian Bone Amulet
+	(4425,6696,29.0711, 1, 0, 1, 1),-- Nightstalker Bow
+	(4425,6697,26.2505, 1, 0, 1, 1),-- Batwing Mantle
+	(4425,11394,13.2757, 1, 0, 1, 1),-- Bat Heart
+	(4425,11393,12.7868, 1, 0, 1, 1),-- Small Bat Skull
+	(4425,11391,12.7492, 1, 0, 1, 1),-- Spined Bat Wing
+	(4425,11392,11.9970, 1, 0, 1, 1),-- Severed Bat Claw
+	(4425,5635,1.6171, 1, 0, 1, 1),-- Sharp Claw
+	(4425,5801,1.5043, 1, 0, 1, 1),-- Kraul Guano
+	(4425,5637,1.2035, 1, 0, 1, 1),-- Large Fang
+	(4425,1529,0.4137, 1, 0, 1, 1),-- Jade
+	(4425,14219,0.1880, 1, 0, 1, 1),-- Geomancer's Cloak
+	(4425,3197,0.1880, 1, 0, 1, 1); -- Stonecutter Claymore
+    
+DELETE FROM `creature_loot_template` WHERE `entry`=50339;
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+	(50339,87217,19.9013, 1, 0, 1, 1),-- Small Bag of Goods
+	(50339,87598,11.2421, 1, 0, 1, 1),-- Monstrous Silk Gloves
+	(50339,87602,11.2037, 1, 0, 1, 1),-- Grain Warden's Gauntlets
+	(50339,87600,11.1708, 1, 0, 1, 1),-- Marshsong Gloves
+	(50339,87596,11.0666, 1, 0, 1, 1),-- Mudmug's Mitts
+	(50339,87603,11.0557, 1, 0, 1, 1),-- Thunderfall Gauntlets
+	(50339,87595,10.9240, 1, 0, 1, 1),-- Gloves of Burrow Spelunking
+	(50339,87601,10.7869, 1, 0, 1, 1),-- Plough Driving Grips
+	(50339,87597,10.7595, 1, 0, 1, 1),-- Gloves of Congealed Mist
+	(50339,87599,10.7321, 1, 0, 1, 1),-- Grower's Gloves
+	(50339,86569,9.7560, 1, 0, 1, 1),-- Crystal of Insanity
+	(50339,93194,0.6636, 1, 0, 1, 1); -- Blood-Soaked Invitation
